7bafc32e213dc7f709e944178fb095ee463a3bec
victorlimaalpha <acessovladm@gmail.com>
2024-11-03 21:13:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-pizzaJson.map((item, index)=>{
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
-    //preeencher as informaçoes em pizza item
+const c = (el)=> document.querySelector(el);
+const cs = (el)=> document.querySelectorAll(el);
 
-    document.querySelector('.pizza-area').append( pizzaItem );
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    //preeencher as informaçoes em pizza item
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+   
+
+    c('.pizza-area').append( pizzaItem );
 });
